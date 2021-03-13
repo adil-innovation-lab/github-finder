4c9ca2f9cdb8761b8018fb3f62c53766ba8de94c
adil-innovation-lab <khanadilaltaf@gmail.com>
2021-03-13 00:13:37 -0800

    Fixed warnings

--- src/context/alert/alertReducer.js
+++ src/context/alert/alertReducer.js
@@ -1,6 +1,6 @@
 import { SET_ALERT, REMOVE_ALERT } from '../types';
 
-export default (state, action) => {
+const alertReducer = (state, action) => {
     switch(action.type) {
         case SET_ALERT:
             return action.payload
@@ -10,3 +10,5 @@ export default (state, action) => {
             return state;
     }
 }
+
+export default alertReducer;
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -1,6 +1,6 @@
 import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
 
-export default (state, action) => {
+const githubReducer = (state, action) => {
     switch(action.type) {
         case SEARCH_USERS: 
             return {
@@ -36,3 +36,5 @@ export default (state, action) => {
             return state;
     }
 }
+
+export default githubReducer;
